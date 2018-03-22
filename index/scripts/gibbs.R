b6c4e24837619d80f133f8337ae67a89743b4611
jamescwu <james.chen.wu@gmail.com>
2018-03-22 01:08:42 -0400

    update gibbs general function

--- index/scripts/gibbs.R
+++ index/scripts/gibbs.R
@@ -1,12 +1,6 @@
 library(MASS)
 
-### data
-X = matrix() #input data, what exactly is X
-y = vector() #what is y
-W = matrix() #what exactly is w
-n = 0 #what exacltyis n
-
-gibbs_sampler = function (X, y, W, n_ij, S = 1000){
+gibbs_sampler = function (X, y, W, S = 1000){
   m = nrow(X)
   ### prior values 
   nu_0 = 2
@@ -18,25 +12,77 @@ gibbs_sampler = function (X, y, W, n_ij, S = 1000){
   ### starting values
   set.seed(1)
   BETAs = list()
-  SIGMAs = list()
-  BETAs[1] = beta = X # what initial starting value for X
-  INV_SIGMAs[1] = inv_sigma2 = 1 / sigma2_0 # what initial starting value for inv_sigma/use inv_sigma or sigma 
+  INV_SIGMAs = list()
+  beta = beta_0
+  BETAs[[1]] = beta
+  inv_sigma2 = 1 / sigma2_0
+  INV_SIGMAs[[1]] = inv_sigma2
   ### Gibbs sampling
   for(s in 2:S) {
     
     # generate a new Beta value from its full conditional
-    Sigma_n = inv ( ( t(X) %*% W %*% X ) / ( 1 / inv_sigma2 ) + inv(W_0) ) 
-    beta_n = Sigma_n %*% ( ( t(X) %*% W %*% X ) %*% y * ( 1 / inv_sigma2 ) + inv(W_0) %*% beta_0)
-    beta = mvrnorm( 1, beta_n, Sigma_n ) # how to sample MVN, use MASS?
+    Sigma_n = ginv ( ( t(X) %*% ginv(W) %*% X ) / ( 1 / inv_sigma2 ) + ginv(W_0) ) 
+    beta_n = Sigma_n %*% ( ( t(X) %*% ginv(W) %*% y ) / ( 1 / inv_sigma2 ) + ginv(W_0) %*% beta_0)
+    beta = mvrnorm( 1, beta_n, Sigma_n ) 
     
     # generate a new 1/sigma2 value from its full conditional
-    SSR_W = ( inv( y - X %*% beta ) %*% W  %*% ( y - X %*% beta ))
-    inv_sigma2 = rgamma(1, ( nu_0 + n_ij )/2, ( nu_0 * sigma2_0 + SSR_W) / 2)
+    SSR_W = ( ginv( y - X %*% beta ) %*% W  %*% ( y - X %*% beta ))
+    inv_sigma2 = rgamma(1, ( nu_0 + m )/2, ( nu_0 * sigma2_0 + SSR_W) / 2)
     
-    BETAs[s] = beta
-    INV_SIGMAs[s] = inv_sigma2
+    BETAs[[s]] = beta
+    INV_SIGMAs[[s]] = inv_sigma2
   }
-  return (BETAs = BETAs, INV_SIGMAs = INV_SIGMAs)
+  return (list(BETAs = BETAs, INV_SIGMAs = INV_SIGMAs))
 }
 
-###
+#testing gibbs sampler
+# X = matrix(rnorm(m * m, mean=0, sd=1), m, m) 
+# W = diag(x = rnorm(m, mean=0, sd=1), nrow = m, ncol = m)
+# y = matrix(rnorm(m * 1, mean=0, sd=1), m, 1) 
+# 
+# 
+# X = matrix(1, m, m) 
+# W = diag(1, nrow = m, ncol = m)
+# y = matrix(1, m, 1) 
+# 
+# 
+# gibbs_sampler(X, y, W, 100)
+
+##FULL procedure
+
+#initialize sigma_i  taU_j as the overall sd 
+Y = readRDS("data/means_SB.rds")
+M = readRDS("data/freqs.rds")
+m = nrow(Y)
+n = ncol(Y)
+
+
+#calculate W matrix
+sigma_i = sd(Y, na.rm = TRUE)
+tau_j = sd(Y, na.rm = TRUE)
+
+W = diag(sigma_i, nrow = m, ncol = m) # m x m
+for (i in 1:m){
+  for (j in 1:n){
+    #how to divide by n_ij when the dimensions are different? whats the iteration for j
+  }
+}
+
+# for (s in 1:S){
+#   X =  matrix(nrow = m, ncol = r) ##CODE FOR SETTING X, X IS m x r but how do i get R
+#   for (i in 1:m){
+#     W = 
+#   } 
+# }
+
+
+
+
+
+# 1. Initialize $\sigma_i$ and $\tau_j$ as the overall standard deviation of the $Y^{(k)}$ matrix. 
+# 2. Simulate $u_i$ and $\sigma_i$ using the generalized Gibbs Sampler. Set random values 
+#     for the starting value of $X$, the algorithm will naturally converge to the true values of $v_j$.
+# 3. Simulate $v_j$ and $\tau_j$ using the generalized Gibbs Sampler. 
+#     Set random values for the starting value of $X$, the algorithm will naturally converge to the true values of $u_i$.
+# 4. Fill in the missing values $y_{ij}$ in $Y^{(k)}$ by sampling from the normal 
+#     distribution $$y_{ij} \sim N(u_i^Tv_j, \frac{\sigma_i^2\tau_j^2}{\sqrt{(n_ij)}})$$

fb7b7fe4b50e8ece29833aa102f8281fdba36ac5
jamescwu <james.chen.wu@gmail.com>
2018-04-01 04:06:20 -0400

    finish full procedure

--- index/scripts/gibbs.R
+++ index/scripts/gibbs.R
@@ -7,7 +7,7 @@ gibbs_sampler = function (X, y, W, sigma2_0 = 1, S = 1000){
   nu_0 = 2
   beta_0 = numeric(r)  
   gamma2 = 100
-  W_0 = diag(r) * gamma2 #W_0 is r x r
+  S_0 = diag(r) * gamma2 #S_0 is r x r
   
   ### starting values
   set.seed(1)
@@ -20,13 +20,13 @@ gibbs_sampler = function (X, y, W, sigma2_0 = 1, S = 1000){
   ### Gibbs sampling
   for(s in 2:S) {
     # generate a new Beta value from its full conditional
-    Sigma_n = ginv ( ( t(X) %*% ginv(W) %*% X ) / ( 1 / inv_sigma2 ) + ginv(W_0) ) 
-    beta_n = Sigma_n %*% ( ( t(X) %*% ginv(W) %*% y ) / ( 1 / inv_sigma2 ) + ginv(W_0) %*% beta_0)
+    Sigma_n = ginv ( ( t(X) %*% ginv(W) %*% X ) / ( 1 / inv_sigma2 ) + ginv(S_0) ) 
+    beta_n = Sigma_n %*% ( ( t(X) %*% ginv(W) %*% y ) / ( 1 / inv_sigma2 ) + ginv(S_0) %*% beta_0)
     beta = mvrnorm( 1, beta_n, Sigma_n ) 
     
     # generate a new 1/sigma2 value from its full conditional
     SSR_W = ( t( y - X %*% beta ) %*% ginv(W)  %*% ( y - X %*% beta ))
-    inv_sigma2 = rgamma(1, ( nu_0 + n )/2, ( nu_0 * sigma2_0 + SSR_W) / 2)
+    inv_sigma2 = rgamma( 1, ( nu_0 + n )/2, ( nu_0 * sigma2_0 + SSR_W) / 2)
     
     BETAs[s,] = beta
     INV_SIGMA2s = c(INV_SIGMA2s, inv_sigma2)
@@ -57,28 +57,28 @@ sigma_post = mean(sqrt(1/INV_SIGMA2s))
 plot(beta, lm(y~ -1+ X)$coef)
 abline(0,2, col = "red")
 
-
+# > sigma
+# [1] 0.5
+# > sigma_post
+# [1] 0.5267057
+# > beta
+# [1]  0.8960932 -0.7614885 -1.1147130 -0.7686936 -0.3393229
+# > beta_post
+# [1]  0.8956598 -0.7162321 -1.0892373 -0.8033861 -0.3284342
 
 #as you increase n you should get closer to the truth, as 
 #you decrease the effects of sigma*W*e you should get closer to the truth
 
-#run the algorithm more than 10 times, check that the beta posterior mean (column means of the beta matrix object)
-#get a distribution of betas, calculate the posterior mean of those, get a distribution of sigma2 (make sure it is, dont use variance of 1)
-
-
-
 ##FULL procedure
-
 Y = readRDS("data/means_SB.rds")
 M = readRDS("data/freqs.rds")
 m = nrow(Y)
 n = ncol(Y)
 
-S = 100 #number of iterations
-
+S = 10 #number of iterations
 ##Set initial Values
-#initialize U and V w/ anova and svd
-mu = sum(Y, na.rm = TRUE)/n
+mu = mean(Y, na.rm = TRUE) #overall mean
+psi = sd(Y, na.rm = TRUE) #overall sd
 a_i = rowMeans(Y, na.rm = TRUE)
 b_j = colMeans(Y, na.rm = TRUE)
 for (i in 1:m){
@@ -88,14 +88,19 @@ for (i in 1:m){
     }
   }
 }
-svd_Y = svd(Y)
-U = svd_Y$u
-V = svd_Y$v
+
+# #initialize U and V w/ anova and svd
+# svd_Y = svd(Y)
+# U = svd_Y$u
+# V = svd_Y$v
+# initialize U and V w/ latent factors, r = 5 how r is selected
+r = 5
+U = matrix(rnorm(m * r, mean=mu, sd=psi), m, r)
+V = matrix(rnorm(n * r, mean=mu, sd=psi), n, r)
 
 #initialize sigmas  taus as the overall sd 
 SIGMA = sqrt(rep(sd(Y, na.rm = TRUE), m))
-TAU = sqrt(rep(sd(Y, na.rm = TRUE), n)) 
-
+TAU = sqrt(rep(sd(Y, na.rm = TRUE), n))
 
 ##Repeat Imputation
 for (s in 1:S){
@@ -105,42 +110,30 @@ for (s in 1:S){
     ##w_jj = tau_j^2/s[i,j]
     for (j in 1:n){
       W[j,j] = W[j,j] * TAU[j]^2
-      if (M[i,j] != 0){ #WHAT HAPPENS WHEN IT IS EQUAL TO 0???
+      if (M[i,j] != 0){ 
         W[j,j] = W[j,j] / M[i,j]
       }
-      # for (j in 1:n){
-      #   if (M[i,j] != 0 && i == j){
-      #     W[i,j] = W[i,j] / M[i,j]
-      #   }
-      # }
     }
     y = Y[i,]
-    S_U = 1
+    S_U = 3
     PHI  = gibbs_sampler(V, y, W, SIGMA[i], S_U)
     U[i,] = PHI$BETAs[S_U,]
     SIGMA[i] = sqrt(PHI$INV_SIGMA2s[S_U])
   }
   #impute V
-  for (j in 1:m){
+  for (j in 1:n){
     W = diag(1, nrow = m, ncol = m) 
-    #WHAT TO DO WHEN M IS NOT SQUARE AND NEED TO GET OBSERVATIONS??
-    ##w_jj = tau_j^2/s[i,j]
     for (i in 1:m){ 
       W[i,i] = W[i,i] * SIGMA[i]^2
-      if (M[i,j] != 0){ #WHAT HAPPENS WHEN IT IS EQUAL TO 0???
-        W[i,i] = W[i,i] / M[i,j] # IS THIS RIGHT?
+      if (M[i,j] != 0){
+        W[i,i] = W[i,i] / M[i,j]
       }
-      # for (j in 1:n){
-      #   if (M[i,j] != 0 && i == j){
-      #     W[i,j] = W[i,j] / M[i,j]
-      #   }
-      # }
     }
     y = Y[,j]
-    S_V = 1
-    PHI  = gibbs_sampler(U, y, W, SIGMA[s], S_V)
+    S_V = 3
+    PHI  = gibbs_sampler(U, y, W, SIGMA[j], S_V)
     V[j,] = PHI$BETAs[S_V,]
-    SIGMA[i] = sqrt(PHI$INV_SIGMAs[S_V])
+    TAU[j] = sqrt(PHI$INV_SIGMA2s[S_V])
   }
   #impute y
   for (i in 1:m){
@@ -156,7 +149,8 @@ for (s in 1:S){
   }
 }
 
-
+#How to select the number of latent factors?
+#How to evaluate the quality of the output
 
 
 
